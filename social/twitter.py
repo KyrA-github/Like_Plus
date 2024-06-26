43e8ed4c236a5cb446891d2f81dfb56473a77a0d
KyrA-github <korenovsostav@gmail.com>
2024-06-26 21:35:36 +0300

    full update

---
+++ social/twitter.py
@@ -0,0 +1,28 @@
+# social/twitter.py
+from selenium.webdriver.common.by import By
+import time
+
+class Twitter:
+    def __init__(self, browser, login, password):
+        self.browser = browser
+        self.login = login
+        self.password = password
+
+    def entrance(self):
+        self.browser.get('https://twitter.com/login')
+        user_login = self.browser.find_element(By.NAME, 'session[username_or_email]')
+        user_login.send_keys(self.login)
+        
+        user_password = self.browser.find_element(By.NAME, 'session[password]')
+        user_password.send_keys(self.password)
+
+        button_login = self.browser.find_element(By.CSS_SELECTOR, 'div[data-testid="LoginForm_Login_Button"]')
+        button_login.click()
+
+        time.sleep(5)
+
+    def likes(self, link):
+        self.browser.get(link)
+        button_like = self.browser.find_element(By.CSS_SELECTOR, 'div[data-testid="like"]')
+        button_like.click()
+        time.sleep(10)
